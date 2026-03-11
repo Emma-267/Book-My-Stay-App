d9497ebfc8825ef7ac704378798fab02f51b6bb2
Emma-267 <ek3833@srmist.edu.in>
2026-03-11 10:18:04 +0530

    Update and rename UseCase1HotelBookingApp.java to BookMyStayApp.java

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,6 @@
 //Establish a clear and predictable starting point for the Hotel Booking application by demonstrating how a Java program begins execution and produces console output.
 //@version 1.0
-public class UseCase1HotelBookingApp{
+public class BookMyStayApp{
     public static void main(String[] args){
         System.out.println("Welcome to the Hotel Booking System");
         System.out.println("System initialized successfully.");
