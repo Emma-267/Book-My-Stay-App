1c630e9cbfa0dff7d878ae1aa8510af494cb30c8
Emma-267 <ek3833@srmist.edu.in>
2026-03-11 09:03:03 +0530

    UC2: Basic Room Types & Static Availability

--- src/UseCase1HotelBookingApp.java
+++
@@ -1,8 +0,0 @@
-//Establish a clear and predictable starting point for the Hotel Booking application by demonstrating how a Java program begins execution and produces console output.
-//@version 1.0
-public class UseCase1HotelBookingApp{
-    public static void main(String[] args){
-        System.out.println("Welcome to the Hotel Booking System");
-        System.out.println("System initialized successfully.");
-    }
-}
